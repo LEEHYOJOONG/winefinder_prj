7e0f2c81067da64f5591043e9512f5e98467658e
hyojoong2 <hjlee@ad-hero.co.kr>
2021-12-14 12:33:54 +0900

    업데이트

--- winefinder_prj/settings.py
+++ winefinder_prj/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9#v)%@!oj@1icfb3^lljachcuajayy4m9inx0)ifcv2se!u%ab'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
